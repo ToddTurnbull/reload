7293925baf208433f228e25ca22c740ab24aa8b1
Todd Turnbull <shingaling@gmail.com>
2016-05-12 15:58:33 -0400

    Remove lines duplicated by pg.sql

--- pg-create-table.sql
+++ pg-create-table.sql
@@ -1,10 +1,3 @@
-drop schema if exists tempdb cascade;
-create schema if not exists tempdb;
-set search_path to tempdb;
-
-drop domain if exists tempdb.multiocc cascade;
-create domain tempdb.multiocc as text;
-
 create table Data (
   RecordNumber varchar(5) not null unique,
   InternalMemo multiocc null,
